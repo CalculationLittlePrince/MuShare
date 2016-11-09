a114572c6f2e0d0cfc3e421f792d40055c553ce5
leeif <geeklyf92610@gmail.com>
2016-11-09 19:09:39 +0900

    Write Player JS File

--- static/js/player/main.js
+++ static/js/player/main.js
@@ -10,11 +10,26 @@ require.config({
   },
 });
 
-require(['jquery', ''], function ($, player) {
+require(['jquery', '../player/player'], function ($, player) {
   $(document).ready(function () {
       window.addEventListener("message", function(event){
         player.loadSongList(JSON.parse(event.data), $(''));
       }, false);
+      var option = {
+        audio: null,
+        list: [],
+        current: null,
+        playpause: null,
+        next: null,
+        preview: null,
+        cover: null,
+        author: null,
+        album: null
+      };
+      player.play(option);
+      $('.btn-volume').click(function(){
+        player.volume(option);
+      });
     }
   );
 });
--- static/js/player/player.js
+++ static/js/player/player.js
@@ -4,11 +4,45 @@
  */
 
 define([], function(){
-  var loadSongList = function(list,songListElement){
+  var loadSongList = function(list,player){
+    if(!player.list) {
+      player.list = [];
+    }
+    player.list.unshift(list);
+  };
+
+  var play = function(player) {
+    console.log('play music');
+    var song = player.current ? player.current : player.list[0];
+    var audio = new Audio('http://kolber.github.io/audiojs/demos/mp3/juicy.mp3');
+    audio.play();
+    player.audio = audio;
+    audio.ontimeupdate = function() {
+      console.log(audio.currentTime);
+    };
+  };
+
+  var puase = function(player) {
 
   };
 
+  var next = function(player) {
+
+  };
+
+  var preview = function(player) {
+
+  };
+
+  var volume = function(player) {
+    if(player.audio) {
+      player.audio.volume  = 0.5;
+    }
+  }
+
   return {
+    play: play,
+    volume: volume,
     loadSongList: loadSongList
   };
 });
