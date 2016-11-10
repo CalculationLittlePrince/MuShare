2c61ea78a161b79fcba5b490e548549b8289d5b8
leeif <geeklyf92610@gmail.com>
2016-11-10 21:21:19 +0900

    Complete MPlayer JS

--- static/js/player/main.js
+++ static/js/player/main.js
@@ -10,25 +10,103 @@ require.config({
   },
 });
 
-require(['jquery', '../player/player'], function ($, player) {
+require(['jquery', '../player/player'], function ($, M) {
   $(document).ready(function () {
-      window.addEventListener("message", function(event){
-        player.loadSongList(JSON.parse(event.data), $(''));
-      }, false);
       var option = {
-        audio: null,
-        list: [],
-        current: null,
-        playpause: null,
-        next: null,
-        preview: null,
-        cover: null,
-        author: null,
-        album: null
+        compare: function (currentSong, list) {
+          var i;
+          for (i = 0; i < list.length; i++) {
+            if (list[i].mid == currentSong.mid) {
+              return i;
+            }
+          }
+        }
       };
-      player.play(option);
-      $('.btn-volume').click(function(){
-        player.volume(option);
+      var songList = [{
+        mid: 1,
+        url: 'http://kolber.github.io/audiojs/demos/mp3/juicy.mp3',
+        title: 'song1',
+        author: 'Lee Band',
+        album: 'zhao zi ji'
+      }, {
+        mid: 2,
+        url: 'http://kolber.github.io/audiojs/demos/mp3/juicy.mp3',
+        title: 'song2',
+        author: 'Lee Band',
+        album: 'zhao zi ji'
+      }, {
+        mid: 3,
+        url: 'http://kolber.github.io/audiojs/demos/mp3/juicy.mp3',
+        title: 'song3',
+        author: 'Lee Band',
+        album: 'zhao zi ji'
+      }];
+      var mplayer = new M.MPlayer(option);
+      mplayer.setOnMetaLoadedListener(function (meta) {
+        $('.pl-container-inner .right-container .author').text(meta.author);
+        $('.pl-container-inner .right-container .album').text(meta.album);
+        $('.pl-control .time .rest').text(meta.duration);
+        $('.pl-control .song-title').text(meta.title);
+      });
+      mplayer.setOnPlayPauseListener(function (paused) {
+        var playpauseEle = $('.pl-control .btn-playpause');
+        if (paused) {
+          //play
+          if (playpauseEle.hasClass('ion-play')) {
+            playpauseEle.removeClass('ion-play');
+          }
+          if (!playpauseEle.hasClass('ion-pause')) {
+            playpauseEle.addClass('ion-pause');
+          }
+        }
+        else {
+          //pause
+          if (playpauseEle.hasClass('ion-pause')) {
+            playpauseEle.removeClass('ion-pause');
+          }
+          if (!playpauseEle.hasClass('ion-play')) {
+            playpauseEle.addClass('ion-play');
+          }
+        }
+      });
+      mplayer.setOnProgressListener(function (progress) {
+        $('.pl-control .time .rest').text(progress.rest);
+        $('.pl-control .progress .progress-player').width(progress.percent + '%');
+      });
+      mplayer.setOnSongListChangeListener(function (songList) {
+        var i;
+        var songListEle = $('.pl-container-inner .song-list');
+        songListEle.find('.item').remove();
+        for (i = 0; i < songList.length; i++) {
+          var jqobj = $('<div class="item">' +
+            songList[i].title +
+            '</div>');
+          songListEle.append(jqobj);
+        }
+      });
+      mplayer.setOnEndedListener(function (){
+        $('.pl-control .progress .progress-player').width('0%');
+      });
+
+      mplayer.insertSongList(songList);
+
+      //DOM event
+      window.addEventListener("message", function (event) {
+        mplayer.insertSongList(JSON.parse(event.data));
+      }, false);
+      $('.pl-control .btn-playpause').click(function () {
+        mplayer.playpause();
+      });
+      $('.pl-control .btn-next').click(function () {
+        mplayer.next();
+      });
+      $('.pl-control .btn-preview').click(function () {
+        mplayer.preview();
+      });
+      $('.pl-control .progress').click(function (e) {
+        var start = $(this).offset().left, end = $(this).offset().right;
+        var percent = (e.pageX - start) / $(this).width();
+        mplayer.seek(percent);
       });
     }
   );
--- static/js/player/player.js
+++ static/js/player/player.js
@@ -3,46 +3,158 @@
  * Created by lee on 2016/11/9.
  */
 
-define([], function(){
-  var loadSongList = function(list,player){
-    if(!player.list) {
-      player.list = [];
-    }
-    player.list.unshift(list);
+define([], function () {
+  function MPlayer(option) {
+    this.songList = option.songList ? option.songList : [];
+    this.audio = null;
+    this.currentSong = null;
+    this.listener = {};
+    this.compare = option.compare;
+  }
+
+  MPlayer.prototype.setOnPlayPauseListener = function (listener) {
+    this.listener.onPlayPause = listener;
   };
 
-  var play = function(player) {
-    console.log('play music');
-    var song = player.current ? player.current : player.list[0];
-    var audio = new Audio('http://kolber.github.io/audiojs/demos/mp3/juicy.mp3');
-    audio.play();
-    player.audio = audio;
-    audio.ontimeupdate = function() {
-      console.log(audio.currentTime);
+  MPlayer.prototype.setOnProgressListener = function (listener) {
+    this.listener.onProgress = listener;
+  };
+
+  MPlayer.prototype.setOnMetaLoadedListener = function (listener) {
+    this.listener.onMetaLoaded = listener;
+  };
+
+  MPlayer.prototype.setOnSongListChangeListener = function (listener) {
+    this.listener.onSongListChange = listener;
+  };
+
+  MPlayer.prototype.setOnEndedListener = function (listener) {
+    this.listener.onEnded = listener;
+  };
+
+  MPlayer.prototype.insertSongList = function (list) {
+    var i;
+    for(i = list.length - 1; i >= 0; i--) {
+      this.songList.unshift(list[i]);
+    }
+    //play first song in list
+    this.load();
+    if (this.listener.onSongListChange) {
+      this.listener.onSongListChange(this.songList);
+    }
+  };
+
+  MPlayer.prototype.load = function (song) {
+    console.log('load music');
+    if (this.songList.length == 0) {
+      return;
+    }
+    this.currentSong = song ? song : this.songList[0];
+    if (this.audio && !this.audio.paused) {
+      //stop current song
+      this.audio.pause();
+      this.audio = null;
+      if(this.listener.onEnded) {
+        this.listener.onEnded();
+      }
+    }
+    this.audio = new Audio(this.currentSong.url);
+    this.setAudioListener();
+    this.playpause();
+  };
+
+  MPlayer.prototype.setAudioListener = function () {
+    //load metadata
+    var self = this;
+    this.audio.onloadedmetadata = function () {
+      var meta = {
+        duration: timeConvert(self.audio.duration),
+        cover: self.currentSong.cover,
+        author: self.currentSong.author,
+        album: self.currentSong.album,
+        title: self.currentSong.title,
+      };
+      if (self.listener.onMetaLoaded) {
+        self.listener.onMetaLoaded(meta);
+      }
+    };
+
+    //progress
+    this.audio.ontimeupdate = function () {
+      if (self.listener.onProgress) {
+        self.listener.onProgress({
+          percent: self.audio.currentTime / self.audio.duration * 100,
+          rest: timeConvert(self.audio.duration - self.audio.currentTime)
+        });
+      }
+    };
+
+    this.audio.onended = function() {
+      if(self.listener.onEnded) {
+        self.listener.onEnded();
+      }
+      self.next();
     };
   };
 
-  var puase = function(player) {
-
-  };
-
-  var next = function(player) {
-
-  };
-
-  var preview = function(player) {
-
-  };
-
-  var volume = function(player) {
-    if(player.audio) {
-      player.audio.volume  = 0.5;
+  MPlayer.prototype.playpause = function () {
+    console.log('playpause');
+    if (this.listener.onPlayPause) {
+      this.listener.onPlayPause(this.audio.paused);
     }
+    if (this.audio && !this.audio.paused) {
+      console.log('pause');
+      this.audio.pause();
+    } else if (this.audio && this.audio.paused) {
+      console.log('play');
+      this.audio.play();
+    }
+  };
+
+  MPlayer.prototype.next = function () {
+    var nextIndex = 0;
+    if (this.compare) {
+      nextIndex = (this.compare(this.currentSong, this.songList) + 1) % this.songList.length;
+    }
+    console.log(nextIndex);
+    this.load(this.songList[nextIndex]);
+  };
+
+  MPlayer.prototype.preview = function () {
+    var previewIndex = 0;
+    if (this.compare) {
+      previewIndex = (this.compare(this.currentSong, this.songList) - 1) % this.songList.length;
+    }
+    if (previewIndex < 0) {
+      previewIndex = this.songList.length - 1;
+    }
+    console.log(previewIndex);
+    this.load(this.songList[previewIndex]);
+  };
+
+  MPlayer.prototype.seek = function (percent) {
+    if(this.audio) {
+      this.audio.currentTime = this.audio.duration * percent;
+    }
+  };
+
+  MPlayer.prototype.volume = function (volume) {
+    if (this.audio) {
+      this.audio.volume = volume;
+    }
+  };
+
+  function timeConvert(time) {
+    var min;
+    var sec;
+    min = Math.floor(time / 60);
+    min = min >= 10 ? '' + min : '0' + min;
+    sec = Math.floor(time % 60);
+    sec = sec >= 10 ? '' + sec : '0' + sec;
+    return min + ':' + sec;
   }
 
   return {
-    play: play,
-    volume: volume,
-    loadSongList: loadSongList
+    MPlayer: MPlayer
   };
 });
